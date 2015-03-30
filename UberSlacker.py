95663ddf9039a9212d3f17ce34a1c57fe724152e
Andrew Koller <akoller>
2015-03-29 23:37:08 -0700

    Added settings_template

--- UberSlacker.py
+++ UberSlacker.py
@@ -12,6 +12,3 @@ parameters = {
 message = "Uber surge pricing is now " + str(surge) + "x"
 response = requests.post(url, params = parameters, data = message)
 # data = response.json()
-print(response.status_code)
-print(response.text)
-print(response)
---
+++ settings_template.py
@@ -0,0 +1,5 @@
+uber_token = 'UBER_SERVER_TOKEN'
+work_address = 'WORK_ADDRESS' #111 Main St, Anywhere, KS
+slack_token = 'SLACK_BOT_TOKEN'
+slack_url = 'https://SUBDOMAIN.slack.com/services/hooks/slackbot' #
+slack_channel = '#uber' #Assumes you created an uber channel
